5d2e9a0d9aabd6d08fde6e12eff0e3dd6df59574
duran221 <cristianfernandoduran@gmail.com>
2019-12-24 23:22:17 -0500

    Actualización dia 24/12/19

--- 20.Constructores/Constructores/Constructores/Program.cs
+++ 20.Constructores/Constructores/Constructores/Program.cs
@@ -6,7 +6,48 @@ namespace Constructores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Al instanciar una clase, estoy en pocas palabras, invocando su constructor para que el objeto sea construído:
+            Coche pichirilo = new Coche();
+            Console.WriteLine(pichirilo.GetInformacion());
+
+            Coche pichirilo2 = new Coche(3, 2.1);
+            Console.WriteLine(pichirilo2.GetInformacion());
+
+
         }
+    }
+
+
+    public class Coche
+    {
+        private int ruedas;
+        private double altura;
+        private double largo;
+        private double ancho;
+        private bool aireAcondicionado;
+
+        public Coche()
+        {
+            this.ruedas = 4;
+            this.altura = 2;
+            this.largo = 1.2;
+            this.ancho = 0.2;
+            this.aireAcondicionado = true;
+        }
+
+        public Coche(double altura,double largo)
+        {
+            this.ruedas = 4;
+            this.altura = altura;
+            this.largo = largo;
+            this.ancho = 0.2;
+            this.aireAcondicionado = true;
+        }
+
+        /// <summary>
+        /// Indica el estado actual del coche así como el contenido de algunas de sus variables.
+        /// </summary>
+        /// <returns>Un String con el estado de las variables del coche actualmente instanciado</returns>
+        public string GetInformacion() => $"Num ruedas: {this.ruedas}, altura: {this.altura}, largo del coche: {this.largo}, alto: {this.ancho}";      
     }
 }
