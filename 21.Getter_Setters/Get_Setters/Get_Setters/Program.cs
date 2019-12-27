2b073e3cfa171c9ee9e2889800eb408ecaeb62e2
duran221 <cristianfernandoduran@gmail.com>
2019-12-27 01:28:26 -0500

    actualización tutoriales fecha 27-12-19

---
+++ 21.Getter_Setters/Get_Setters/Get_Setters/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Get_Setters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto auto1 = new Auto();
+
+            auto1.Ruedas = 3;
+            Console.WriteLine(auto1.Ruedas);
+            Console.WriteLine(auto1.retornarCosto());
+
+        }
+    }
+
+    /// <summary>
+    /// Estableciendo una clase parcial con la cual podemos dividir una clase en trozos de código
+    /// </summary>
+    partial class Auto
+    {
+        //Auto-propiedades que nos permiten gestionar tanto set- como getters, muy útil para ahorrar costo en lineas de código
+        public int Ruedas { get; set; }
+        //Inicializando una Auto-propiedad
+        public int valvulas { get; set; } = 16;
+        public bool AireCondicionado { get; set; }
+        public string tapiceria { get; set; }
+
+        public Auto()
+        {
+            this.Ruedas = 4;
+            this.AireCondicionado = false;
+            this.tapiceria = "Cuero";
+
+        }
+
+    }
+
+    /// <summary>
+    /// Otro trozo de código perteneciente a la clase Auto:
+    /// </summary>
+    partial class Auto
+    {
+        public double retornarCosto()
+        {
+            return 12.3*this.Ruedas;
+        }
+
+    }
+}
