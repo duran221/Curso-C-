ed2eb9e42515da42f16aea7737ea32e5b657e094
duran221 <cristianfernandoduran@gmail.com>
2019-12-29 19:12:58 -0500

    Actualización del curso día 29-12-19

---
+++ 25.Clases Anónimas/ClasesAnonimas/ClasesAnonimas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClasesAnonimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var claseAnonima1 = new {
+                Nombre="Cristian",
+                edad= 25,
+                carrera= "Ingeniería de sistemas"
+            };
+
+            var claseAnonima2 = new { 
+                Nombre= "Juan Carlos",
+                edad= 20,
+                carrera= "Ingeniería de sistemas"
+            
+            };
+
+            Console.WriteLine($"Datos de la clase anónima 1: Nombre {claseAnonima1.Nombre}, edad {claseAnonima1.edad}, carrera {claseAnonima1.carrera}");
+
+            claseAnonima1 = claseAnonima2;
+
+            Console.WriteLine($"Datos de la clase anónima 1: Nombre {claseAnonima1.Nombre}, edad {claseAnonima1.edad}, carrera {claseAnonima1.carrera}");
+
+        }
+
+    }
+}
---
+++ 26.Arrays-Vectores/Arrays/Arrays/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades = new int[2];
+
+            edades[0] = 12;
+            edades[1] = 11;
+
+            int[] anios = { 2, 3, 4, 5 };
+
+            Console.WriteLine(edades[0]);
+            Console.WriteLine(anios[0]);
+
+            //Así se declara un array implícito:
+            var implicito = new[] {2,2.5,3.2,0.5 };
+            
+            var implicito2 = new[] {"Nombre1", "Nombre2", "Nombre3"};
+
+            //Declarando un array de objetos
+            Persona[] salon = new Persona[2];
+            salon[0] = new Persona("Cris", 22);
+
+            //Declarando e inicializando un array de tipos o clases anónimas:
+            var arrayClaseAnonima = new[]
+            {
+                new {velocidad=300,marca= "Chevrolet" },
+                new {velocidad=200,marca= "Renault" },
+                new {velocidad=330,marca= "Renault" }
+            };
+
+            Console.WriteLine(arrayClaseAnonima[0]);
+            
+
+
+            
+        }
+    }
+
+    class Persona
+    {
+        string nombre { get; set; }
+        int edad { get; set; }
+
+
+        public Persona(string nombre,int edad) {
+
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+    }
+}
